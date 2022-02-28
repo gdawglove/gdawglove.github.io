f3ebbab5d3e3e9a6a6d4c95e2a0c4bd3225bca04
Mihir <mihirsuks@gmail.com>
2022-02-28 17:23:15 -0600

    Smirk

---
+++ CMakeFiles/jin.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/mihir/Coding/gdog/libs/lodepng/lodepng.c" "/home/mihir/Coding/gdog/CMakeFiles/jin.dir/libs/lodepng/lodepng.c.o"
+  "/home/mihir/Coding/gdog/src/core/core.c" "/home/mihir/Coding/gdog/CMakeFiles/jin.dir/src/core/core.c.o"
+  "/home/mihir/Coding/gdog/src/core/env/env.c" "/home/mihir/Coding/gdog/CMakeFiles/jin.dir/src/core/env/env.c.o"
+  "/home/mihir/Coding/gdog/src/core/gll/gll.c" "/home/mihir/Coding/gdog/CMakeFiles/jin.dir/src/core/gll/gll.c.o"
+  "/home/mihir/Coding/gdog/src/core/globals.c" "/home/mihir/Coding/gdog/CMakeFiles/jin.dir/src/core/globals.c.o"
+  "/home/mihir/Coding/gdog/src/core/input/input.c" "/home/mihir/Coding/gdog/CMakeFiles/jin.dir/src/core/input/input.c.o"
+  "/home/mihir/Coding/gdog/src/core/logger/logger.c" "/home/mihir/Coding/gdog/CMakeFiles/jin.dir/src/core/logger/logger.c.o"
+  "/home/mihir/Coding/gdog/src/core/thread/thread.c" "/home/mihir/Coding/gdog/CMakeFiles/jin.dir/src/core/thread/thread.c.o"
+  "/home/mihir/Coding/gdog/src/core/window/window.c" "/home/mihir/Coding/gdog/CMakeFiles/jin.dir/src/core/window/window.c.o"
+  "/home/mihir/Coding/gdog/src/driver.c" "/home/mihir/Coding/gdog/CMakeFiles/jin.dir/src/driver.c.o"
+  "/home/mihir/Coding/gdog/src/gfx/gfx.c" "/home/mihir/Coding/gdog/CMakeFiles/jin.dir/src/gfx/gfx.c.o"
+  "/home/mihir/Coding/gdog/src/gfx/model.c" "/home/mihir/Coding/gdog/CMakeFiles/jin.dir/src/gfx/model.c.o"
+  "/home/mihir/Coding/gdog/src/gfx/shader.c" "/home/mihir/Coding/gdog/CMakeFiles/jin.dir/src/gfx/shader.c.o"
+  "/home/mihir/Coding/gdog/src/gfx/sprite.c" "/home/mihir/Coding/gdog/CMakeFiles/jin.dir/src/gfx/sprite.c.o"
+  "/home/mihir/Coding/gdog/src/gfx/texture.c" "/home/mihir/Coding/gdog/CMakeFiles/jin.dir/src/gfx/texture.c.o"
+  "/home/mihir/Coding/gdog/src/resm/resm.c" "/home/mihir/Coding/gdog/CMakeFiles/jin.dir/src/resm/resm.c.o"
+  "/home/mihir/Coding/gdog/src/snd/snd.c" "/home/mihir/Coding/gdog/CMakeFiles/jin.dir/src/snd/snd.c.o"
+  "/home/mihir/Coding/gdog/src/snd/wav_util.c" "/home/mihir/Coding/gdog/CMakeFiles/jin.dir/src/snd/wav_util.c.o"
+  "/home/mihir/Coding/gdog/src/stm/states.c" "/home/mihir/Coding/gdog/CMakeFiles/jin.dir/src/stm/states.c.o"
+  "/home/mihir/Coding/gdog/src/stm/stm.c" "/home/mihir/Coding/gdog/CMakeFiles/jin.dir/src/stm/stm.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "src"
+  "libs"
+  "C:/cpplib"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
